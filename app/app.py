7680ed8645c212d092dbaf02fb2426d0f346aee6
Soumyadip Sasmal <your-github-email@gmail.com>
2026-05-03 00:31:39 +0530

    add docker

--- app/app.py
+++ app/app.py
@@ -1,16 +1,14 @@
-cat > app/app.py << 'EOF'
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask DevOps Project Running 🚀"
+    return "Flask DevOps Project Running"
 
-@app.route('/health')
+@app.route("/health")
 def health():
     return {"status": "healthy"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-EOF
